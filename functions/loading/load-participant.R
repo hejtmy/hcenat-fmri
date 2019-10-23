fdf519ce10f310110de47f8c5153f94c58fc8c0a
hejtmy <hejtmy@gmail.com>
2019-10-23 16:24:51 +0200

    Adds loading of multiple participants

--- functions/loading/load-participant.R
+++ functions/loading/load-participant.R
@@ -1,3 +1,43 @@
+#' Title
+#'
+#' @param data_dir 
+#' @param ids 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+load_participants <- function(data_dir, ids=c()){
+  df_preprocessing <- load_participant_preprocessing_status()
+  participants <- list()
+  if(length(ids) != 0) df_preprocessing <- df_preprocessing %>% filter(ID %in% ids)
+  for(i in 1:nrow(df_preprocessing)){
+    line <- df_preprocessing[i,]
+    participant_data <- list()
+    if(line$session1_ok){
+      data <- load_participant(data_dir, participant, 1)
+      participant_data[[1]] <- data[[1]]
+    }
+    if(line$session2_ok){
+      data <- load_participant(data_dir, participant, 2)
+      participant_data[[2]] <- data[[1]]
+    }
+    participants[[line$ID]] <- participant_data
+  }
+  return(participants)
+}
+
+
+#' Loads participant data from given directory when given ID and a MRI session
+#'
+#' @param data_dir 
+#' @param id 
+#' @param sessions 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 load_participant <- function(data_dir, id, sessions = c(1,2)){
   response <- list()
   for(session in sessions){
