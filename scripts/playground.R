3f6a7eaa1823a950ada93b99937303135995875f
hejtmy <hejtmy@gmail.com>
2019-10-23 23:11:29 +0200

    Cleans out pointing results

--- scripts/playground.R
+++ scripts/playground.R
@@ -13,7 +13,7 @@ df_quests <- df_quests_info(session$quests_logs)
 df_player <- add_pulses_player(df_quests, session$quests_logs, session$player_log)
 
 df_quests <- df_quests_info(data[[1]]$quests_logs)
-pointing_results(df_quests, data[[1]]$quests_logs, data[[1]]$player_log, correct_angles)
+pointing_results(data[[1]]$quests_logs, data[[1]]$player_log, correct_angles)
 
 quest <- get_quest(data[[1]]$quests_logs, 12)
 quest_summary(quest, data[[1]]$player_log)
@@ -42,7 +42,7 @@ participant <- load_participants(data_dir, c("HCE_K_23"), df_preprocessing)
 quest <- get_quest(participant$HCE_K_23[[1]]$quests_logs, 13)
 get_correct_angle(quest, participant$HCE_K_23[[1]]$player_log)
 plot_quest_path.participant(participant$HCE_K_23[[2]], 3, img_path)
-
+quest_pointing_accuracy(quest, participant$HCE_K_23[[1]]$player_log)
 ## investigation of pulses ----
 # difference between 1st and last
 sum(diff(participant$HCE_E_14[[1]]$player_log %>% filter(Input == "fMRISynchro") %>% .$Time))
