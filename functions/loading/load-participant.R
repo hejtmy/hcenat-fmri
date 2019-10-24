cb1b3df671b7a30c941a06c14fa19babffede4ca
hejtmy <hejtmy@gmail.com>
2019-10-24 15:23:06 +0200

    Modifies loading to only iunclude given sessions

--- functions/loading/load-participant.R
+++ functions/loading/load-participant.R
@@ -7,21 +7,23 @@
 #' @export
 #'
 #' @examples
-load_participants <- function(data_dir, ids=c(), df_preprocessing = NULL){
+load_participants <- function(data_dir, ids=c(), df_preprocessing = NULL, sessions = c(1,2)){
   if(is.null(df_preprocessing)) df_preprocessing <- load_participant_preprocessing_status()
   participants <- list()
   if(length(ids) != 0) df_preprocessing <- df_preprocessing %>% filter(ID %in% ids)
   for(i in 1:nrow(df_preprocessing)){
-    line <- df_preprocessing[i,]
+    line <- df_preprocessing[i, ]
     participant_data <- list()
-    if(line$session1_ok){
+    message("Loading participant ", line$ID)
+    if(line$session1_ok & (1 %in% sessions)){
       data <- load_participant(data_dir, line$ID, 1)
       participant_data[[1]] <- data[[1]]
     }
-    if(line$session2_ok){
+    if(line$session2_ok & (2 %in% sessions)){
       data <- load_participant(data_dir, line$ID, 2)
       participant_data[[2]] <- data[[2]]
     }
+    if(length(participant_data) == 0) next
     participants[[line$ID]] <- participant_data
   }
   return(participants)
