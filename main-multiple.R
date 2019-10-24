0f0dea0ccd5d3591d61733c58844610dc7225cc2
hejtmy <hejtmy@gmail.com>
2019-10-24 21:01:07 +0200

    Slight update for the main muliiple pipeline

--- main-multiple.R
+++ main-multiple.R
@@ -1,6 +1,7 @@
 library(data.table)
 library(navr)
 library(dplyr)
+library(ez)
 sapply(list.files("functions", full.names = T, recursive = T), source)
 CORRECT_ANGLES <- read.table("data/correct-angles.csv", sep=",", header=TRUE)
 
@@ -24,3 +25,18 @@ fmri <- restructure_mri(components)
 df_pointing <- pointing_results.participants(participants)
 
 ## fmri analysis ----
+component_names <- c("filt_cen_11", "filt_cen_16", "filt_cen_23", "filt_cen_37", 
+                     "filt_cen_39", "filt_cen_46", "filt_dmn_2", "filt_dmn_51", 
+                     "filt_dmn_52","filt_hpc_12", "filt_hpc_54", "filt_hpc_56", 
+                     "filt_mot_33", "filt_sn_31",  "filt_sn_8", "filt_unk_10", 
+                     "filt_vis_4", "filt_vis_43")
+pointing_pulses <- create_pointing_pulses_table(df_pointing)
+pointing_fmri <- get_fmri(fmri, pointing_pulses)
+
+head(pointing_fmri)
+ezANOVA(pointing_fmri,
+        dv = "filt_cen_16",
+        wid = participant,
+        within = correct)
+
+ggplot(pointing_fmri, aes(participant, filt_cen_11, fill=correct)) + geom_boxplot()
