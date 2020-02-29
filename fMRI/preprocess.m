9c40d2234bc8b576a399c4749e9fc9c129861a3f
hejtmy <hejtmy@gmail.com>
2020-02-29 17:11:52 +0100

    Adds matlab preprocessing pipeline

--- fMRI/preprocess.m
+++ fMRI/preprocess.m
@@ -18,6 +18,7 @@ pointingData = filtervalidevents(pointingData);
 %% Setting FMRI base
 TR = 3; % TR = 3 sec
 SESSION_LENGTH = 400;
+MIN_PULSE_RATIO = 0.9; %minimum ratio of a pulse happening in event
 blankTs = zeros(400,1); % blank time series
 hrf = spm_hrf(TR); % (SPM Toolbox required) create hrf function 
 
@@ -25,5 +26,21 @@ hrf = spm_hrf(TR); % (SPM Toolbox required) create hrf function
 i = 1;
 [subject, ~] = getsubjectnamesession(subs{i});
 subjectData = getsubjectevents(walkingData, subject);
+%% Movement ------
 movingTimes = geteventtimes(subjectData, 'moving');
 movementBlocks = eventtimestotrblocks(movingTimes, TR, SESSION_LENGTH);
+movementBlocks = movementBlocks > MIN_PULSE_RATIO;
+hrfMovement = convolveblockhrf(movementBlocks, TR);
+
+%% Still ------
+stillTimes = geteventtimes(subjectData, 'still');
+stillBlocks = eventtimestotrblocks(stillTimes, TR, SESSION_LENGTH);
+stillBlocks = stillBlocks > MIN_PULSE_RATIO;
+hrfStill  = convolveblockhrf(movementBlocks, TR);
+
+%% Pointing ------
+subjectData = getsubjectevents(pointingData, subject);
+pointingTimes = geteventtimes(subjectData);
+pointingBlocks = eventtimestotrblocks(stillTimes, TR, SESSION_LENGTH);
+pointingBlocks = pointingBlocks > MIN_PULSE_RATIO;
+hrfPoinitng  = convolveblockhrf(pointingBlocks, TR);
