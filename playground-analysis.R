6c6582fa03563a0fdaf31bb6fcabca83c191ccdf
hejtmy <hejtmy@gmail.com>
2020-06-06 01:20:49 +0200

    Adds outline of multiple participant analysis

--- playground-analysis.R
+++ playground-analysis.R
@@ -77,7 +77,6 @@ df_regressions_moving_learn_trial <- data.frame()
 for(component_name in names(comps)){
   regressions_moving_learn_trial[[component_name]] <- lm(comps[[component_name]] ~ participant_series$moving.learn + participant_series$moving.trial)
   out <- broom::tidy(regressions_moving_learn_trial[[component_name]])
-  
   out <- out %>%
     select(term, estimate, p.value) %>%
     filter(term != "(Intercept)") %>%
@@ -88,4 +87,27 @@ for(component_name in names(comps)){
   df_regressions_moving_learn_trial <- rbind(df_regressions_moving_learn_trial, out)
 }
 
-df_regressions_moving_learn_trial
+### All participants ------
+df_hrfs <- data.frame()
+for(name in good_participants){
+  out <- as.data.frame(hrfs[[name]])
+  out$participant <- name
+  out$pulse_id <- 1:400
+  df_hrfs <- rbind(df_hrfs, out)
+}
+all_data <- merge(df_hrfs, fmri, by=c("participant", "pulse_id"))
+
+summary(lm(filt_dmn_52 ~ moving.learn + moving.trial, data=all_data))
+
+
+all_data %>%
+  filter(participant %in% good_participants[1:10]) %>%
+  ggplot(aes(filt_cen_16, moving)) + 
+    geom_point() + geom_smooth(method="lm") +
+    facet_wrap(~participant)
+
+l <- lmerTest::lmer(filt_dmn_52 ~ moving.learn + moving.trial + (moving|participant), data=all_data)
+summary(l)
+
+
+
