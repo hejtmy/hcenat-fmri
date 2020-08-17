8df93064fda426e315c6667cc1caeb3ce7b9d46f
Lukáš Hejtmánek <hejtmy@gmail.com>
2020-08-17 16:24:38 +0200

    Adds script to do some basic data checks

---
+++ data-checks.R
@@ -0,0 +1,67 @@
+library(car)
+library(navr)
+library(plotly)
+library(knitr)
+library(nlme)
+library(tidyverse)
+
+sapply(list.files("functions", full.names = TRUE, recursive = TRUE), source)
+data_dir <- "E:/OneDrive/NUDZ/projects/HCENAT/Data/"
+
+source("scripts/load-data.R")
+
+## Pointing ------
+# Checking that pointing HRFS correspond
+plot_pointing_hrfs <- function(hrfs, df_behavioral, participant){
+  hrf <- hrfs[[participant]]
+  ggplot(data=data.frame()) +
+    geom_line(aes(1:400, y=hrf$pointing), size=2) +
+    geom_line(aes(1:400, y=hrf$`pointing-learn`), color="red") +
+    geom_line(aes(1:400, y=hrf$`pointing-trial`), color="blue") +
+    geom_vline(data=filter(df_behavioral, ID==participant),
+               aes(xintercept=quest_pulse_start, color = type)) +
+    scale_color_manual(values=c("learn" = "red", "trial"="blue"))
+}
+plot_pointing_hrfs(hrfs, df_behavioral, "HCE_E_14")
+plot_pointing_hrfs(hrfs, df_behavioral, "HCE_K_4")
+
+#' The missing pointings are the pointings which encompassed less then 0.75 percent of the 
+#' pulse and therefore weren't modeled in the HRF modelling part in matlab
+
+## Movement ------
+
+plot_movement_hrfs <- function(hrfs, df_behavioral, participant){
+  hrf <- hrfs[[participant]]
+  ggplot(data=data.frame()) +
+    #geom_line(aes(1:400, y=hrf$moving), size=2) +
+    geom_line(aes(1:400, y=hrf$`moving-learn`), color="red") +
+    geom_line(aes(1:400, y=hrf$`moving-trial`), color="blue") +
+    geom_vline(data=filter(df_behavioral, ID==participant),
+               aes(xintercept=quest_pulse_start, color = type)) +
+    scale_color_manual(values=c("learn" = "red", "trial"="blue"))
+}
+
+plot_movement_hrfs(hrfs, df_behavioral, "HCE_E_14")
+plot_movement_hrfs(hrfs, df_behavioral, "HCE_K_4")
+plot_movement_hrfs(hrfs, df_behavioral, "HCE_E_9")
+
+
+hrf <- as.data.frame(hrfs$HCE_E_14) %>%
+  select(moving.learn, moving, moving.trial) %>%
+  mutate(pulse_id = 1:N_PULSES, ID = "HCE_E_14") %>%
+  left_join(df_pulses, by=c("ID", "pulse_id")) %>%
+  pivot_longer(cols=c(moving.learn:moving.trial))
+ggplot(hrf, aes(pulse_id, value, color=name)) + geom_line()
+
+filter(hrf, name == "moving") %>%
+  ggplot(aes(pulse_id, value, color=learn, group="ALL")) +
+  geom_line(size=1.25)
+
+## Visualising movement types with overlap
+#' 1 = only one is active
+#' 0 = no třype of trial is running
+#' 2 both are seemingly active
+filter(hrf, name == "moving") %>%
+  ggplot(aes(pulse_id, value, color=factor(learn+trial), group="ALL")) +
+  geom_line(size=1.25)
+
