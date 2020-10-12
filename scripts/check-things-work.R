f08a4bca58599254fa21d53a246b63f6df2edc33
Lukáš Hejtmánek <hejtmy@gmail.com>
2020-10-12 23:11:51 +0200

    Adds checks thatt particiapnts are stationary during pointing

--- scripts/check-things-work.R
+++ scripts/check-things-work.R
@@ -1,9 +1,10 @@
 library(tidyverse)
+source("scripts/loading.R")
 
 PARTICIPANT_CODE <- "HCE_E_10"
 
 load("participants-prepared.RData")
-df_results <- read_table("exports/participant-performance.csv", 
+df_results <- read.table("exports/participant-performance.csv", 
                          sep = ";", header = TRUE)
 
 df_preprocessing <- load_participant_preprocessing_status()
@@ -20,10 +21,23 @@ df_onsets_stops <- read.table(file.path("exports", "walking.csv"),
 df_onsets_stops <- df_onsets_stops %>%
   filter(fmri_code == FMRI_CODE)
 
-onset <- df_onsets_stops %>% filter(movement_type=="moving") %>% .[20,]
+onset <- df_onsets_stops %>% filter(movement_type == "moving") %>% .[20,]
 
 df_player %>%
   filter(fmri_time > onset$time, fmri_time < onset$time+onset$duration) %>%
   ggplot(aes(x=Time, y=distance)) + geom_line()
 
 df_onsets[1,]
+
+## Check the pulses ----
+df_pulses <- read.table("exports/participant-pulses.csv", sep = ";", 
+                        header = TRUE)
+df_pulses <- filter(df_pulses, fmri_code == FMRI_CODE)
+i_still <- df_pulses %>%
+  filter(is_pointing) %>%
+  pull(pulse_id)
+
+# participant should be stationary during pointing
+df_player %>%
+  filter(pulse_id %in% i_still) %>%
+  ggplot(aes(Position.x, Position.z)) + geom_point()
